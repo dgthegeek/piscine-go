51ec8247842070426e910409146ea1587b610b91
dgaye <damegaye1887@gmail.com>
2022-11-03 17:45:06 +0000

    third commit of printcomb

--- printcomb.go
+++ printcomb.go
@@ -4,16 +4,16 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
 	for a := '0'; a <= '9'; a++ {
-		for b := a + 1; b <= 9; b++ {
-			for c := b + 1; c <= 9; c++ {
+		for b := a + 1; b <= '9'; b++ {
+			for c := b + 1; c <= '9'; c++ {
 				z01.PrintRune(a)
 				z01.PrintRune(b)
 				z01.PrintRune(c)
-				if a == 8 {
+				if a == 7 {
 					break
 				}
 			}
 		}
-		z01.PrintRune('\n')
 	}
+	z01.PrintRune('\n')
 }
