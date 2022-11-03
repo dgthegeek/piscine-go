13f9fc4225f10fbdbed2bfefc4b3ad54f0c1a01e
dgaye <damegaye1887@gmail.com>
2022-11-03 15:17:27 +0000

    first commit of printalphabet

---
+++ printalphabet/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "github.com/01-edu/z01"
+
+func main() {
+	var aRune string = "abcdefghijklmnopqrstuvwxyz"
+
+	for i := 0; i <= 25; i++ {
+		z01.PrintRune(rune(aRune[i]))
+	}
+	z01.PrintRune('\n')
+}
