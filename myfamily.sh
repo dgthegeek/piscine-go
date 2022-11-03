5ef5b70a32fa320cc19b429de905e2adfc7c172e
dgaye <damegaye1887@gmail.com>
2022-11-03 14:11:31 +0000

    Commit number 4 of cl-camp4

--- myfamily.sh
+++ myfamily.sh
@@ -1,1 +1,1 @@
-curl -s https://learn.zone01dakar.sn/assets/superhero/all.json | jq -r --arg id "$HERO_ID" ' .[] | select( .id=='$id') | .connections.relatives' | sed 's/\"//g'
+curl -s https://learn.zone01dakar.sn/assets/superhero/all.json | jq ' .[] | select( .id=='$HERO_ID') | .connections.relatives' | sed "s/\"//g"
