cd79bfa0d468eb01b3eadb478401ba92ce807961
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-04 01:13:14 +0000

    Update number 3 printcomb.go

--- printcomb2.go
+++ printcomb2.go
@@ -1,15 +1,15 @@
 package piscine
 
-import "fmt"
+import "github.com/01-edu/z01"
 
 func PrintComb2() {
     for a := 0; a <= 98; a++ {
 		for b := 1; b <= 99; b++ {
 			if a<b {
 				if a==98 && b==99  {
-					fmt.Printf("%02d %02d\n",a,b)
+					z01.PrintRune("%02d %02d\n",a,b)
 				}else{
-					fmt.Printf("%02d %02d, ",a,b)
+					z01.PrintRune("%02d %02d, ",a,b)
 				}
 			}
 		}
