a622fd498e85fc30df7c60ae2579a2bb6d0fb1d8
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-04 08:38:16 +0000

    Upload files to ''

--- printcomb2.go
+++ printcomb2.go
@@ -1,20 +1,17 @@
 package piscine
 
-import "github.com/01-edu/z01"
+import "fmt"
 
 func PrintComb2() {
-for a := 0; a <= 98; a++ {
-	for b := 1; b <= 99; b++ {
-		if a<b {
-			if a!=98 && b!=99  {
-				fmt.Printf("%02d %02d, ",a,b)
-			
-				}else if a==98 && b==99 {
-				fmt.Printf("%02d %02d\n",a,b)
-				
-	
+	for a := 0; a <= 98; a++ {
+		for b := 1; b <= 99; b++ {
+			if a < b {
+				if a != 98 && b != 99 {
+					fmt.Printf("%02d %02d, ", a, b)
+				} else if a == 98 && b == 99 {
+					fmt.Printf("%02d %02d\n", a, b)
 				}
 			}
-		}		
+		}
 	}
 }
