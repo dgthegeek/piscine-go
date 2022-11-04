833fa5d2b3f72c6fa3b927727aacf38def91d672
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-04 00:52:19 +0000

    Update number 2

--- printcomb2.go
+++ printcomb2.go
@@ -6,11 +6,10 @@ func PrintComb2() {
 	for a := 0; a <= 98; a++ {
 		for b := 1; b <= 99; b++ {
 			if a<b {
-				if a!=98 && b!=99  {
-					fmt.Printf("%02d %02d, ",a,b)
-				}else if a==98 && b==99 {
+				if a==98 && b==99  {
 					fmt.Printf("%02d %02d\n",a,b)
-					
+				}else{
+					fmt.Printf("%02d %02d, ",a,b)
 				}
 			}
 		}
