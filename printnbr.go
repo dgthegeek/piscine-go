0bfb14fde06cfa960c7bfb1067cf345e67d245b0
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-05 01:00:46 +0000

    second commit printnbr

--- printnbr.go
+++ printnbr.go
@@ -5,8 +5,8 @@ import "github.com/01-edu/z01"
 func PrintNbr(n int){
     if n<0{
         x := n*(-1)
-       z01.PrintRune('-')
-       z01.PrintRune(x)
+        z01.PrintRune('-')
+        z01.PrintRune(x)
     }else if n>0{
         fz01.PrintRune(n)
     }else{
