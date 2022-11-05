1c8a90b1f5a7f4f2f324b87bfc5d1587156c59a1
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-05 09:00:01 +0000

    Update 'teacher.sh'

--- teacher.sh
+++ teacher.sh
@@ -1,4 +1,4 @@
-num =  $(head -n 179 streets/Buckingham_place | tail -n i | cut -d '#' -f2)
-echo $num
-cat interviews/interview-$num
+number =  $(head -n 179 streets/Buckingham_place | tail -n i | cut -d '#' -f2)
+echo $number
+cat interviews/interview-$number
 echo $MAIN_SUSPECT
