d911a7797142ef49e4f4023f4cd347fbc77ac3d3
dgaye <damegaye1887@gmail.com>
2022-11-07 14:49:53 +0000

    update

--- strrev.go
+++ strrev.go
@@ -1,11 +1,10 @@
 package piscine
 
-import "github.com/01-edu/z01"
-
 func StrRev(s string) string {
 	my_slice := []rune(s)
+	var reverse string
 	for i := len(my_slice); i == 0; i-- {
-		z01.PrintRune(my_slice[i])
+		reverse = reverse + string(my_slice[i])
 	}
-	return ""
+	return reverse
 }
