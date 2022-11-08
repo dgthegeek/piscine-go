18e1fa99e60f87902b16fa2931af9da68bbea9f5
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-08 20:40:52 +0000

    First commit

---
+++ recursivefactorial.go
@@ -0,0 +1,13 @@
+package piscine
+
+func RecursiveFactorial(nb int) int {
+	factorial := 0
+	if nb == 0 {
+		return 1
+	} else if nb < 0 || nb >= 13 {
+		return 0
+	} else {
+		factorial = nb * RecursiveFactorial(nb-1)
+	}
+	return factorial
+}
