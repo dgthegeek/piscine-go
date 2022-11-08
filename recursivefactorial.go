d2e7fbf8d1c3f8c262b6e42b23e40886ebc1cd2e
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-08 20:46:14 +0000

    retrying

--- recursivefactorial.go
+++ recursivefactorial.go
@@ -5,10 +5,11 @@ func RecursiveFactorial(nb int) int {
 
 	if nb == 0 {
 		return 1
-	} else if nb < 0 || nb >= 13 {
+	} else if nb < 0 || nb > 13 {
 		return 0
 	} else {
 		factorial = nb * RecursiveFactorial(nb-1)
 	}
 	return factorial
 }
+
