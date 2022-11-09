3d5e498df708eb038111f11b22697a702559f251
dgaye <damegaye1887@gmail.com>
2022-11-09 10:23:22 +0000

    Understood my error now

--- findnextprime.go
+++ findnextprime.go
@@ -1,30 +1,13 @@
 package piscine
 
-func Prime(nb int) bool {
-	if nb <= 1 {
-		return false
-	}
-	for i := 2; i < nb; i++ {
-		if nb%i == 0 {
-			return false
-		}
-	}
-	return true
-}
-
 func FindNextPrime(nb int) int {
-	result := 0
-	if Prime(nb) {
-		return nb
-	} else {
-		n := nb + 1
-		for i := n; i < (n + 10); i++ {
-			if Prime(n) {
-				result += n
-			} else {
-				n++
-			}
-		}
-		return n
+	if nb <= 1 {
+		return 0
 	}
+	for i := 2; i*i <= nb; i++ {
+		if nb%i == 0 {
+			return FindNextPrime(nb + 1)
+		}
+	}
+	return nb
 }
