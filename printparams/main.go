1c161822cddcd357bfd93e74ca6cfd6a754dc557
dgaye <damegaye1887@gmail.com>
2022-11-11 10:15:56 +0000

    get the names

---
+++ printparams/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func main() {
+	args := os.Args
+	for _, arg := range args[1:] {
+		for i, r := range arg {
+			if i > 1 {
+				z01.PrintRune(r)
+			}
+		}
+		z01.PrintRune('\n')
+	}
+}
