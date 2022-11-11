4b18e64dd8e2d9d40d36d9863b273ae1234c0581
dgaye <damegaye1887@gmail.com>
2022-11-11 05:40:25 +0000

    Capitalize

---
+++ capitalize.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func alpha(a rune) bool {
+	if (a >= 'A' && a <= 'Z') || (a >= 'a' && a <= 'z') || (a >= '0' && a <= '9') {
+		return true
+	}
+	return false
+}
+
+func Capitalize(s string) string {
+	slice := []rune(s)
+	vide := true
+	for i := 0; i < len(s); i++ {
+		if alpha(slice[i]) && vide {
+			if slice[i] >= 'a' && slice[i] <= 'z' {
+				slice[i] -= 32
+			}
+			vide = false
+		} else if slice[i] >= 'A' && slice[i] <= 'Z' {
+			slice[i] += 32
+		} else if !alpha(slice[i]) {
+			vide = true
+		}
+	}
+	return string(slice)
+}
+
+func main() {
+	fmt.Println(Capitalize("Hello! How are you? How+are+things+4you?"))
+}
