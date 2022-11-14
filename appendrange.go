9a48184b720a295fcd57afadcb6a4d3987a18cfe
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-14 23:18:09 +0000

    Upload files to ''

--- appendrange.go
+++ appendrange.go
@@ -4,11 +4,11 @@ func AppendRange(min, max int) []int {
 	var slice []int
 	if min >= max {
 		return slice
+
 	} else {
 		for i := min; i < max; i++ {
 			slice = append(slice, i)
 		}
 	}
 	return slice
-
 }
