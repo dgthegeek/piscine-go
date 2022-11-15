2e69cad06b441464c5b675af7032710408a5d16c
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-15 00:19:01 +0000

    Upload files to ''

--- appendrange.go
+++ appendrange.go
@@ -1,13 +1,16 @@
 package piscine
 
 func AppendRange(min, max int) []int {
+
 	var slice []int
 	if min >= max {
 		return slice
 
 	} else {
 		for i := min; i < max; i++ {
+
 			slice = append(slice, i)
+
 		}
 	}
 	return slice
