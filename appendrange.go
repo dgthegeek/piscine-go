ae65279769fd0e4cf1d1f47a3cc696738ff4ea36
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-15 11:51:36 +0000

    Your go files are not correctly formated

--- appendrange.go
+++ appendrange.go
@@ -1,13 +1,12 @@
 package piscine
 
 func AppendRange(min, max int) []int {
-
-	var resultado []int
+	var result []int
 
 	for i := min - 1; i < max-1; i++ {
-		resultado = append(resultado, i+1)
+
+		result = append(resultado, i+1)
 	}
 
-	return resultado
-
+	return result
 }
