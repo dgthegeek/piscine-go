b722b53cda3df081dc91aa2fa7cc65da764b8459
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-15 11:55:02 +0000

    Getting error 'your go files are not correctly formated'

--- appendrange.go
+++ appendrange.go
@@ -1,11 +1,12 @@
 package piscine
 
 func AppendRange(min, max int) []int {
+
 	var result []int
 
 	for i := min - 1; i < max-1; i++ {
 
-		result = append(resultado, i+1)
+		result = append(result, i+1)
 	}
 
 	return result
