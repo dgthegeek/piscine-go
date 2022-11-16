f0d2457af3bdd2c49d4d998ae9a132bd67e5ce81
dgaye <damegaye1887@gmail.com>
2022-11-16 16:44:30 +0000

    boolean

---
+++ boolean/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"os"
+
+	"github.com/01-edu/z01"
+)
+
+func printStr(str string) {
+	arrayStr := []rune(str)
+
+	for _, s := range arrayStr {
+		z01.PrintRune(s)
+	}
+	z01.PrintRune('\n')
+}
+
+func even(nbr int) bool {
+	if nbr%2 == 0 {
+		return true
+	}
+	return false
+}
+
+func isEven(nbr int) bool {
+	if even(nbr) == true {
+		return true
+	}
+	return false
+}
+
+func main() {
+	EvenMsg := "I have an even number of arguments\n"
+	OddMsg := "I have an odd number of arguments\n"
+
+	lenOfArg := len(os.Args) - 1
+
+	if isEven(lenOfArg) {
+		printStr(EvenMsg)
+	} else {
+		printStr(OddMsg)
+	}
+}
