ea5d5b62afebb60e18e8171626b77978a1d02a99
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-16 00:59:54 +0000

    first commit

---
+++ split.go
@@ -0,0 +1,18 @@
+package piscine
+
+func Split(s, sep string) string {
+
+	lists := []rune(s)
+	n1 := len(lists)
+
+	listsep := []rune(sep)
+	n2 := len(listsep)
+
+	for i := 0; i < n1-n2; i++ {
+		if sep == s[i:n2+i] {
+			s = s[:i] + " " + s[n2+i:]
+			n1 = n1 - n2
+		}
+	}
+	return SplitWhiteSpaces(s)
+}
