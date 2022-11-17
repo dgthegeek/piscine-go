aef035f911e9deabd5d4a326b8854400c9cc03f9
dgaye <damegaye1887@gmail.com>
2022-11-17 07:08:15 +0000

    retrying

--- cat/main.go
+++ cat/main.go
@@ -1,18 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
+
+	"github.com/01-edu/z01"
 )
+
+func PrintStr(s string) {
+	my_slice := []rune(s)
+	for i := 0; i < len(my_slice); i++ {
+		z01.PrintRune(my_slice[i])
+	}
+}
 
 func main() {
 	for i := 1; i < len(os.Args); i++ {
 		content, err := ioutil.ReadFile(os.Args[i])
 		if err != nil {
-			fmt.Println(err.Error())
-			return
+			mes := "ERROR: " + err.Error() + "no such file or directory"
+			PrintStr(mes)
+			z01.PrintRune('\n')
 		}
-		fmt.Printf(string(content))
+		PrintStr(string(content))
+		z01.PrintRune('\n')
 	}
 }
