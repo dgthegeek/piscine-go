d39a75d2f70559471458cdbd458b0e465e57b336
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-17 18:22:47 +0000

    Update 'enigma.go'

--- enigma.go
+++ enigma.go
@@ -1,14 +1,11 @@
 package piscine
 
 func Enigma(a ***int, b *int, c *******int, d ****int) {
-
 	tmpc := *******c
 	*******c = ***a
 	tmpd := ****d
 	****d = tmpc
-
 	tmpb := *b
 	*b = tmpd
-
 	***a = tmpb
 }
