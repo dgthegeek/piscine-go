35dbcff12231eec880aa55dc431e3fa1d42680d7
dgaye <damegaye1887@gmail.com>
2022-11-17 02:07:42 +0000

    3rd commit

--- issorted.go
+++ issorted.go
@@ -5,7 +5,12 @@ func IsSorted(f func(a, b int) int, a []int) bool {
 		if i+1 < len(a) {
 			if f(a[i], a[i+1]) > 0 {
 				return true
-			} else if f(a[i], a[i+1]) < 0 {
+			}
+		}
+	}
+	for i := range a {
+		if i+1 < len(a) {
+			if f(a[i], a[i+1]) < 0 {
 				return true
 			}
 		}
