5f1b8686846469534f0af38f98c6a77c47af2d79
dgaye <damegaye1887@gmail.com>
2022-11-17 02:00:53 +0000

    second commit

--- issorted.go
+++ issorted.go
@@ -3,7 +3,9 @@ package piscine
 func IsSorted(f func(a, b int) int, a []int) bool {
 	for i := range a {
 		if i+1 < len(a) {
-			if f(a[i], a[i+1]) > 0 || f(a[i], a[i+1]) < 0 {
+			if f(a[i], a[i+1]) > 0 {
+				return true
+			} else if f(a[i], a[i+1]) < 0 {
 				return true
 			}
 		}
