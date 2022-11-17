feb5e46412ca7b362e761e780c1aa425757412fc
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-17 22:57:16 +0000

    Add 'fixthemain/main.go'

---
+++ fixthemain/main.go
@@ -0,0 +1,54 @@
+package main 
+
+import "github.com/01-edu/z01"
+
+const (
+	CLOSE = 0
+	OPEN = 1
+)
+
+type Door struct {
+	State int
+}
+
+func PrintStr(str string) {
+	slice := []rune(str)
+	for _, s := range slice {
+		z01.PrintRune(s)
+	}
+}
+
+func CloseDoor(ptrDoor *Door) {
+	PrintStr("Door closing...\n")
+	ptrDoor.State = CLOSE
+}
+
+func IsDoorOpen(ptrDoor *Door) bool {
+	PrintStr("Door is open ?\n")
+	return ptrDoor.State == OPEN
+}
+
+func IsDoorClose(ptrDoor *Door) bool {
+	PrintStr("Door is close ?\n")
+	return ptrDoor.State == CLOSE
+}
+
+func OpenDoor(ptrDoor *Door){
+	PrintStr("Door Opening...\n")
+	ptrDoor.State = OPEN
+}
+
+func main() {
+	var door Door
+
+	OpenDoor(&door)
+	if IsDoorClose(&door) {
+		OpenDoor(&door)
+	}
+	if IsDoorOpen(&door) {
+		CloseDoor(&door)
+	}
+	if door.State == OPEN {
+		CloseDoor(&door)
+	}
+}
