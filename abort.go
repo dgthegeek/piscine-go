c02dd9ebec93e3710af3858ddf89f672f17df0b8
dgaye <damegaye1887@gmail.com>
2022-11-17 12:15:37 +0000

    first commit

--- abort.go
+++ abort.go
@@ -1,5 +1,19 @@
 package piscine
 
+func SortIntegerTable(table []int) {
+	tampon := 0
+	for i := 0; i < len(table)-1; i++ {
+		for j := 0; j < len(table)-1; j++ {
+			if table[j] > table[j+1] {
+				tampon = table[j]
+				table[j] = table[j+1]
+				table[j+1] = tampon
+
+			}
+		}
+	}
+}
+
 func Abort(a, b, c, d, e int) int {
 	arg := []int{a, b, c, d, e}
 	SortIntegerTable(arg)
