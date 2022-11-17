0143498ab5388c9984e47988112cd30eeff2cb3f
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-17 21:57:09 +0000

    Update 'pilot/main.go'

--- pilot/main.go
+++ pilot/main.go
@@ -3,21 +3,15 @@ package main
 import (
 	"fmt"
 
-	student ".."
+	piscine ".."
 )
 
-type Pilot struct {
-	Name     string
-	Life     float32
-	Age      int
-	Aircraft int
-}
-
 func main() {
-	var donnie student.Pilot
+	var donnie piscine.Pilot
 	donnie.Name = "Donnie"
 	donnie.Life = 100.0
 	donnie.Age = 24
-	donnie.Aircraft = student.AIRCRAFT1
+	donnie.Aircraft = 1
+
 	fmt.Println(donnie)
 }
