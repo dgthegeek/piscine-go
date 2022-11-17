283395261751d905514f677e4ced21117ee805f8
dgaye <damegaye1887@gmail.com>
2022-11-17 02:22:19 +0000

    issorted

--- issorted.go
+++ issorted.go
@@ -1,19 +1,19 @@
 package piscine
 
-func IsSorted(f func(int, int) int, a []int) bool {
-	down := false
-	up := false
+func IsSorted(f func(int, int) int, arr []int) bool {
+	up := true
+	down := true
 
-	for i := 1; i < len(a); i++ {
-		if f(a[i-1], a[i]) >= 0 {
-			down = true
+	for i := 1; i < len(arr); i++ {
+		if !(f(arr[i-1], arr[i]) >= 0) {
+			up = false
 		}
 	}
 
-	for i := 1; i < len(a); i++ {
-		if f(a[i-1], a[i]) <= 0 {
-			up = true
+	for i := 1; i < len(arr); i++ {
+		if !(f(arr[i-1], arr[i]) <= 0) {
+			down = false
 		}
 	}
-	return down || up
+	return up || down
 }
