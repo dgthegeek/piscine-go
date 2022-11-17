c131295960d7e41752fc5c501e52734ee58a73b4
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-17 18:13:42 +0000

    swapping

---
+++ enigna.go
@@ -0,0 +1,14 @@
+package piscine
+
+func Enigma(a ***int, b *int, c *******int, d ****int) {
+
+	tmpc := *******c
+	*******c = ***a
+	tmpd := ****d
+	****d = tmpc
+
+	tmpb := *b
+	*b = tmpd
+
+	***a = tmpb
+}
