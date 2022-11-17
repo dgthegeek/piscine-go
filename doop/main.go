693e8732c04399953195f7499c1fbda868161a1c
dgaye <damegaye1887@gmail.com>
2022-11-17 07:34:27 +0000

    retrying

--- doop/main.go
+++ doop/main.go
@@ -4,11 +4,37 @@ import (
 	"os"
 )
 
-func ff() string {
-	args := os.Args[1:]
-	nb1 := atoi(args[0])
+func Atoi(s string) int {
+	tab := []rune(s)
+	a := 0
+	if len(tab) == 0 {
+		return 0
+	}
+	for i := 0; i < len(tab); i++ {
+		if tab[i] == 45 || tab[i] == 43 || (tab[i] >= 48 && tab[i] <= 57) {
+			if i != 0 && (tab[i] == 45 || tab[i] == 43) {
+				return 0
+			}
+			if tab[i] >= 48 && tab[i] <= 57 {
+				a *= 10
+				a = a + int(tab[i]) - '0'
+			}
+
+		} else {
+			return 0
+		}
+	}
+	if tab[0] == 45 {
+		a *= -1
+	}
+
+	return a
+}
+
+func ff(args []string) string {
+	nb1 := Atoi(args[0])
 	signe := args[1]
-	nb2 := atoi(args[2])
+	nb2 := Atoi(args[2])
 
 	// errors
 
@@ -20,60 +46,21 @@ func ff() string {
 		} else if args[1] == "/" && nb2 == 0 {
 			return "No division by 0"
 		} else if args[1] == "+" {
-			return ((nb1 + nb2) + '0')
+			return PrintNbr(nb1 + nb2)
 		} else if args[1] == "-" {
-			return ((nb1 - nb2) + '0')
+			return PrintNbr(nb1 - nb2)
 		} else if args[1] == "*" {
-			return string((nb1 * nb2) + '0')
+			return PrintNbr(nb1 * nb2)
 		} else if args[1] == "/" {
-			return ((nb1 / nb2) + '0')
+			return PrintNbr(nb1 / nb2)
 		} else {
-			return ((nb1 % nb2) + '0')
+			return PrintNbr(nb1 % nb2)
 		}
 	}
 	return "0"
 }
 
 func main() {
-	ff()
+	args := os.Args[1:]
+	ff(args)
 }
-
-// package main
-// import (
-// 	"os"
-// 	"github.com/01-edu/z01"
-// )
-// func main() {
-// 	args := os.Args[1:]
-// 	nb1 := Atoi(args[0])
-// 	signe := args[1]
-// 	nb2 := Atoi(args[2])
-// 	// errors
-// 	mod := "No Modulo by 0"
-// 	Lmod := []rune(mod)
-// 	div := "No division by 0"
-// 	Ldiv := []rune(div)
-// 	if len(args) != 3 || signe != "+" && signe != "*" && signe != "-" && signe != "/" && signe != "%" {
-// 		// it prints nothing
-// 	} else {
-// 		if args[1] == "%" && nb2 == 0 {
-// 			for _, v := range Lmod {
-// 				z01.PrintRune(v)
-// 			}
-// 		} else if args[1] == "/" && nb2 == 0 {
-// 			for _, v := range Ldiv {
-// 				z01.PrintRune(v)
-// 			}
-// 		} else if args[1] == "+" {
-// 			z01.PrintRune((nb1 + nb2) + '0')
-// 		} else if args[1] == "-" {
-// 			z01.PrintRune((nb1 - nb2) + '0')
-// 		} else if args[1] == "*" {
-// 			z01.PrintRune((nb1 * nb2) + '0')
-// 		} else if args[1] == "/" {
-// 			z01.PrintRune((nb1 / nb2) + '0')
-// 		} else {
-// 			z01.PrintRune((nb1 % nb2) + '0')
-// 		}
-// 	}
-// }
