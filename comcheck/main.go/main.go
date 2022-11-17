a54a9b62753acf9378882f1dae805965c483189c
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-17 17:48:46 +0000

    Update 'comcheck/main.go/main.go'

--- comcheck/main.go/main.go
+++ comcheck/main.go/main.go
@@ -1,31 +1,3 @@
-/*package main
-
-import (
-	"fmt"
-	"os"
-)
-
-func main() {
-	var tableauderune []rune
-	var result string
-	parametre := []string{"01", "galaxy", "galaxy 01"}
-
-	for i := 1; i < len(os.Args); i++ {
-		tableauderune = []rune(os.Args[i])
-	}
-
-	for j := 0; j < len(tableauderune); j++ {
-		if tableauderune[j] != ' ' {
-			result += string(tableauderune[j])
-		}
-	}
-
-	for _, s := range parametre {
-		if result == s {
-			fmt.Println("Alert!!!")
-		}
-	}
-}*/
 package main
 
 import (
