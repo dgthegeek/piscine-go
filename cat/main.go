949266a8c1f0313ec15d29d62d878a0852565ee3
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-18 17:40:39 +0000

    retrying

--- cat/main.go
+++ cat/main.go
@@ -1,45 +1,37 @@
 package main
 
 import (
-	"io"
 	"io/ioutil"
 	"os"
-
 	"github.com/01-edu/z01"
+	"io"
 )
 
-func reader(fileName string) string {
-	content, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		return "error"
-	}
-	return string(content)
-}
-
-func print(str string) {
-	for _, val := range str {
-		z01.PrintRune(val)
-	}
-}
-
 func main() {
-	args := os.Args[1:]
+	arguments := os.Args[1:]
 
-	end := false
-	for _, fileName := range args {
-		if _, err := os.Stat(fileName); err != nil {
-			print("ERROR: open " + fileName + ": no such file or directory\n")
-			os.Exit(1)
-			return
-		}
-		print(reader(fileName))
-		end = true
-
-	}
-	if !end {
+	if len(arguments) == 0 {
 		reader := io.TeeReader(os.Stdin, os.Stdout)
 		ioutil.ReadAll(reader)
 		os.Stdin.Close()
 		os.Stdout.Close()
+	} else {
+		for _, arg := range arguments {
+			ReadFile(string(arg))
+		}
+	}
+}
+
+func ReadFile(fileName string) {
+	data, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		for _, val := range "ERROR: " + err.Error() {
+			z01.PrintRune(val)
+		}
+		z01.PrintRune('\n')
+		os.Exit(1)
+	}
+	for _, dt := range data {
+		z01.PrintRune(rune(dt))
 	}
 }
