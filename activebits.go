7db786bc2497b837a3e8a612c1a9310ef7830232
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-18 06:40:24 +0000

    Update 'activebits.go'

--- activebits.go
+++ activebits.go
@@ -1,10 +1,19 @@
 package piscine
 
-func ActiveBits(n int) uint {
-	cnt := 0
-	for n > 0 {
-		cnt += n & 1
-		n >>= 1
+func ActiveBits(n int) int {
+	cntt := 0
+	remain := 0
+	div := 0
+	if n < 0 {
+		n = -n
 	}
-	return uint(cnt)
+	DivMod(n, 2, &div, &remain)
+	for div > 0 {
+		DivMod(n, 2, &div, &remain)
+		n = div
+		if remain == 1 {
+			cntt++
+		}
+	}
+	return int(cntt)
 }
