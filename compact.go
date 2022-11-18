41fabd9a97b05198b0ea0d00f9bffeacc7f98125
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-18 00:21:37 +0000

    Update 'compact.go'

--- compact.go
+++ compact.go
@@ -1,11 +1,11 @@
 package piscine
 
-func Compact(ptr *[]string, length int) int {
-	cnt := 0
-	for i := 0; i < length; i++ {
-		if (*ptr)[i] != " " {
-			cnt++
+func Compact(ptr *[]string, leng int) int {
+	count:=0
+	for i:=0;i<leng;i++{
+		if (*ptr)[i]!=" "{
+			count++
 		}
 	}
-	return cnt
+	return count
 }
