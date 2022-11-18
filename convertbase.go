f62cba78c771201d228e1cf15916704293abf428
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-18 00:03:56 +0000

    Add 'convertbase.go'

---
+++ convertbase.go
@@ -0,0 +1,28 @@
+package piscine
+
+func ConvertBase(nbr, baseForm, baseTo string) string {
+
+	result := ""
+	my_map := map[rune]int{}
+
+	slice1 := []rune(nbr)
+	n1 := len(slice1)
+
+	slice1 := []rune(baseForm)
+	n2 := len(slice2)
+
+	slice3 := []rune(baseTo)
+	n3 := len(slice3)
+
+	power := 1
+	count := 0
+	for i := n1 - 1; i >= 0; i-- {
+		count = count + my_map[[]rune(nbr)[i]]*power
+		power *= n2
+	}
+	for count != 0 {
+		result = string(baseTo[count%n3]) + result
+		count /= n3
+	}
+	return result
+}
