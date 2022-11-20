f4d4a3bca89a25716e1da7164d34e66177ff8253
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-20 01:42:58 +0000

    Update 'sortwordarr.go'

--- sortwordarr.go
+++ sortwordarr.go
@@ -1,26 +1,21 @@
 package piscine
 
 func SortWordArr(array []string) {
-
 	quickSrot2(array, 0, len(array)-1)
 
 }
 
-
 func quickSrot2(table []string, beg int, end int) {
-
 	if beg < end {
 		lockPivo := mudaVariavel2(table, beg, end)
 		quickSrot2(table, beg, lockPivo-1)
 		quickSrot2(table, lockPivo+1, end)
-
 	}
 }
 
 func mudaVariavel2(table []string, beg int, end int) int {
 	pivo := table[end]
 	i := beg - 1
-
 	for j := beg; j < end; j++ {
 		if table[j] <= pivo {
 			i++
@@ -29,25 +24,8 @@ func mudaVariavel2(table []string, beg int, end int) int {
 			table[i] = aux
 		}
 	}
-
 	aux := table[end]
 	table[end] = table[i+1]
 	table[i+1] = aux
-
 	return i + 1
 }
-
-
-
-/*
-	var str string
-	for i := 0; i < Lent3(array); i++ {
-		str = str + array[i]
-
-	}
-	arr := []rune(str)*/
-	//	for p := 0; p < len(arr); p++ {
-
-	//	array[p] = string(arr[p])
-
-	//}
