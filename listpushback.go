1d62aaa0c34fe8343b9fb59ce2549adc9ab29bc3
dgaye <damegaye1887@gmail.com>
2022-11-22 13:43:23 +0000

    push back

--- listpushback.go
+++ listpushback.go
@@ -1,6 +1,4 @@
-package main
-
-import "fmt"
+package piscine
 
 type NodeL struct {
 	Data interface{}
@@ -22,18 +20,5 @@ func ListPushBack(l *List, data interface{}) {
 	} else {
 		l.Tail.Next = n
 		l.Tail = l.Tail.Next
-	}
-}
-
-func main() {
-	link := &List{}
-
-	ListPushBack(link, "Hello")
-	ListPushBack(link, "man")
-	ListPushBack(link, "how are you")
-
-	for link.Head != nil {
-		fmt.Println(link.Head.Data)
-		link.Head = link.Head.Next
 	}
 }
