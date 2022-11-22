2aa9f6df8fb8d9073e6a03ffeaf7ddb5422548f5
dgaye <damegaye1887@gmail.com>
2022-11-22 16:25:37 +0000

    front

---
+++ listpushfront.go
@@ -0,0 +1,15 @@
+package piscine
+
+func ListPushFront(l *List, data interface{}) {
+	n := &NodeL{Data: data}
+
+	if l.Head == nil {
+		l.Head = n
+		l.Tail = n
+		return
+	} else {
+		n.Next = l.Head
+		l.Head = n
+
+	}
+}
