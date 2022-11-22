dfc7802c6a3ded814edc1b85728526828ba52142
dgaye <damegaye1887@gmail.com>
2022-11-22 18:07:04 +0000

    last of the list

--- listlast.go
+++ listlast.go
@@ -1,13 +1,13 @@
 package piscine
 
 func ListLast(l *List) interface{} {
+	if l.Head == nil {
+		return nil
+	}
+
 	element := l.Head
 	for element != nil {
 		element = element.Next
-	}
-
-	if l.Head == nil {
-		return nil
 	}
 
 	return element.Data
