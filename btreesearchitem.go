cbe3aee2f9089521b4befee61d3c9f115363cd9b
dgaye <damegaye1887@gmail.com>
2022-11-23 17:14:04 +0000

    search

---
+++ btreesearchitem.go
@@ -0,0 +1,13 @@
+package piscine
+
+func BTreeSearchItem(root *TreeNode, elem string) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Data > elem {
+		return BTreeSearchItem(root.Right, elem)
+	} else if root.Data < elem {
+		return BTreeSearchItem(root.Left, elem)
+	}
+	return root
+}
