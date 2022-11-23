b8de69f8a7c3c00ce97c8476c716c947602b6cf1
dgaye <damegaye1887@gmail.com>
2022-11-23 15:32:11 +0000

    post order

--- btreeapplypostorder.go
+++ btreeapplypostorder.go
@@ -2,8 +2,8 @@ package piscine
 
 func BTreeApplyPostorder(root *TreeNode, f func(...interface{}) (int, error)) {
 	if root != nil {
+		BTreeApplyPostorder(root.Left, f)
 		BTreeApplyPostorder(root.Right, f)
 		f(root.Data)
-		BTreeApplyPostorder(root.Left, f)
 	}
 }
