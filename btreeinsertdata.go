0270830ddc2f9256cb83238746b0f0427a3fe8a9
dgaye <damegaye1887@gmail.com>
2022-11-23 14:39:44 +0000

    tree

---
+++ btreeinsertdata.go
@@ -0,0 +1,18 @@
+package piscine
+
+type TreeNode struct {
+	Left, Right, Parent *TreeNode
+	Data                string
+}
+
+func BTreeInsertData(root *TreeNode, data string) *TreeNode {
+	if root == nil {
+		return &TreeNode{Data: data}
+	} else if data > root.Data {
+		return BTreeInsertData(root.Left, data)
+	} else if data < root.Data {
+		return BTreeInsertData(root.Right, data)
+	} else {
+		return BTreeInsertData(root.Parent, data)
+	}
+}
