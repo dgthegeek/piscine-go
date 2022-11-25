f3eac4beff81d3386fe6afb248d269d237e5d33f
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-25 17:50:35 +0000

    Add 'btreetransplant.go'

---
+++ btreetransplant.go
@@ -0,0 +1,17 @@
+package piscine
+
+func BTreeTransplant(root, node, rplc *TreeNode) *TreeNode {
+    if root == nil {
+        return nil
+    }
+    nod := node
+    if node.Parent == nil {
+        root = rplc
+    } else if node == node.Parent.Right {
+        nod.Parent.Right = rplc
+    } else {
+        nod.Parent.Left = rplc
+    }
+    nod.Parent = node.Parent
+    return root
+}
