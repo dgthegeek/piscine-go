e70807c194d249c9ecc51592111863fcd3487428
DAME GAYE <dgaye@noreply.learn.zone01dakar.sn>
2022-11-26 16:00:37 +0000

    Update 'atoibase.go'

--- atoibase.go
+++ atoibase.go
@@ -5,8 +5,8 @@ import (
 )
 
 func AtoiBase(s string, str string) int {
-	indx:=0
-	for _,res:= range str {
+	indx := 0
+	for _, res := range str {
 		if string(res) == "-" || string(res) == "+" || strings.Count(str, string(res)) > 1 {
 			indx = 1
 			break
@@ -14,12 +14,12 @@ func AtoiBase(s string, str string) int {
 	}
 
 	if indx == 1 || len(str) < 2 {
-		return 0	
+		return 0
 	} else {
 		fin := 0
-		for i,res:= range s {
-			ind := strings.Index(str,string(res))
-			fin += ind* RecursivePower(len(str),len(s)-1 - i)
+		for i, res := range s {
+			ind := strings.Index(str, string(res))
+			fin += ind* RecursivePower(len(str), len(s)-1 - i)
 		}
 		return fin
 	}
